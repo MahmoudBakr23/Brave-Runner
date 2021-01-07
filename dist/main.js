eb02fb9ac3fee2fa0d7a19c7000bcdf707d15910
Mahmoud Bakr <mbakr6821@gmail.com>
2021-01-08 00:16:36 +0200

    init

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+
+console.log('Here we go');
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['env']
+            }
+        }
+      },
+    ],
+  },
+};
